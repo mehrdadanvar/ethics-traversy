e1d91a8a15c2967312d471a278de09e624ec0ffc
mehrdadanvar <mehrdad.anvar@gmail.com>
2022-05-19 15:51:18 -0700

    started error handling by creating an if function in the middle ware folder which overwrites the default error handling implemented by express. essentailly it helps us in the developement environment to get the full stack of errors when a bad request is sent to the backend.once in production, only the bare minimum messages will be returned. this is controlled by the dotenv file in the projects root directory

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const {errorHandler} =require('../backend/middleware/errorMiddleware');
 
 const port = process.env.PORT || 5000;
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started with port number ${port}`));
