50842fdbeaea66e2c22cfaaa8523d658e9e2cd6b
mehrdadanvar <mehrdad.anvar@gmail.com>
2022-05-19 15:58:17 -0700

    brough in the express async handler and just required it at the top of the controller js file, then added a asyncHandler function to all of the four defined methods

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,37 @@
+const asyncHandler = require('express-async-handler');
 // @desc    Get gaols
 // @route   GET /api/goals
 // @access  Private 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
     res.status(200).json({message:'we are getting gaols here!'})
-};
+});
 
 
 // @desc    Set gaol
 // @route   POST /api/goals
 // @access  Private 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async(req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('please inculde a text field in the request body so that the api return some shit to you')
     }
     res.status(200).json({message:'we are setting gaols here remember it is singular and a post request!'})
-};
+});
 
 // @desc    Update gaol
 // @route   PUT /api/goals/:id
 // @access  Private 
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async(req, res) => {
     res.status(200).json({message:`update goal number ${req.params.id}`})
-};
+});
 
 
 // @desc    Delete gaols
 // @route   DELETE /api/goals/:id
 // @access  Private 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async(req, res) => {
     res.status(200).json({message: `yohoooy we are feleting goal number ${req.params.id}`})
-};
+});
 
 
 
